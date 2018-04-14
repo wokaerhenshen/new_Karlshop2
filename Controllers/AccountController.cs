0a9c2d072e035acab05212028e6ee99851111b79
karl <karlxu0410@gmail.com>
2018-04-13 17:41:55 -0700

    add images

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -95,7 +95,7 @@ namespace new_Karlshop.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-
+                //
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: true);
